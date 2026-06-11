bdfbc80e3ba541c8d05112b591d8371032b3cc40
erictriumph <eric@saferfamily.com>
2026-06-10 23:36:00 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import Head from "next/head";
-
 export const metadata = {
   title: "NGPCX",
   description: "Know before you buy.",
@@ -8,9 +6,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-      </Head>
       <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
